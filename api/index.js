d07231aacc708db851c8c8e3e2833bf520702c3f
Alifian <alif40550@gmail.com>
2025-07-07 20:02:48 +0700

    fix deployment

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import chat from "./controllers/chatbotController.js";
+import chat from "../controllers/chatbotController.js";
 
 dotenv.config();
 
