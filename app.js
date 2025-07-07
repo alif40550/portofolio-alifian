312ae87316463ad93691aaee6b8e57e7d4b3928d
Alifian <alif40550@gmail.com>
2025-07-07 19:36:25 +0700

    fix deployment

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.html");
+});
 
 app.post("/api/chatbot", chat);
 
