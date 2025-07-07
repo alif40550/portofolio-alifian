0379aa7b8e9d4e600aa1b2c81f01a9efb9814a73
Alifian <alif40550@gmail.com>
2025-07-07 19:29:54 +0700

    fix deployment

--- api/index.js
+++ api/index.js
@@ -10,11 +10,6 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.render("index");
-});
 
 app.post("/api/chatbot", chat);
 
